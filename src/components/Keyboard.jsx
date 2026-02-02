36eebaf55bdc385f3b62fd1467a28846be09e208
Prashuna668 <joshiprashuna@gmail.com>
2026-02-02 14:17:21 +0545

    done

---
+++ src/components/Keyboard.jsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useEffect, useContext } from "react";
+import Key from "./key";
+import { AppContext } from "../App";
+function Keyboard() {
+  const keys1 = ["Q","W","E","R","T","Y","U","I","O","P"];
+  const keys2 = ["A","S","D","F","G","H","J","K","L"];
+  const keys3 = ["Z","X","C","V","B","N","M"];
+  const {
+    disabledLetters,
+    currAttempt,
+    gameOver,
+    onSelectLetter,
+    onEnter,
+    onDelete
+  } = useContext(AppContext);
+
+  const handleKeyboard = useCallback((event) => {
+    if (gameOver.gameOver) return;
+
+    if (event.key === "Enter") {
+      onEnter();
+    } 
+    else if (event.key === "Backspace") {
+      onDelete();
+    } 
+    else {
+      const pressed = event.key.toUpperCase();
+      const allKeys = [...keys1, ...keys2, ...keys3];
+
+      if (allKeys.includes(pressed)) {
+        onSelectLetter(pressed);
+      }
+    }
+  }, [currAttempt]);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
+  return (
+    <div className="keyboard">
+      <div className="line1">
+        {keys1.map((key) => (
+          <Key keyVal={key} disabled={disabledLetters.includes(key)} />
+        ))}
+      </div>
+
+      <div className="line2">
+        {keys2.map((key) => (
+          <Key keyVal={key} disabled={disabledLetters.includes(key)} />
+        ))}
+      </div>
+
+      <div className="line3">
+        <Key keyVal={"ENTER"} bigKey />
+        {keys3.map((key) => (
+          <Key keyVal={key} disabled={disabledLetters.includes(key)} />
+        ))}
+        <Key keyVal={"DELETE"} bigKey />
+      </div>
+    </div>
+  );
+}
+
+export default Keyboard;
